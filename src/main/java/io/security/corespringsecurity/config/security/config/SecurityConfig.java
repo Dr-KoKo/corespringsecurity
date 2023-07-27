2ce17ccbebb57c5a884df48f074669bf8f0b543c
Denny-Ko <donghar@naver.com>
2023-07-27 17:32:41 +0900

    7) 로그아웃 및 인증에 따른 화면 보안 처리(2)

--- src/main/java/io/security/corespringsecurity/config/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/config/security/config/SecurityConfig.java
@@ -6,15 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -54,6 +53,11 @@ public class SecurityConfig {
                                 .loginProcessingUrl("/login_proc")
                                 .defaultSuccessUrl("/")
                                 .permitAll()
+                )
+                .logout((logout) ->
+                        logout
+                                .logoutRequestMatcher(AntPathRequestMatcher.antMatcher("/logout"))
+                                .logoutSuccessUrl("/")
                 );
 
         return http.build();
