2ec6c8ef53652cbe3e59bd76c04fad25e4c13831
Denny-Ko <donghar@naver.com>
2023-08-01 05:29:57 +0900

    4) 인증 핸들러 - AjaxAuthenticationSuccessHandler, AjaxAuthenticationFailureHandler

--- src/main/java/io/security/corespringsecurity/config/security/config/AjaxSecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/config/security/config/AjaxSecurityConfig.java
@@ -1,6 +1,8 @@
 package io.security.corespringsecurity.config.security.config;
 
 import io.security.corespringsecurity.config.security.filter.AjaxLoginProcessingFilter;
+import io.security.corespringsecurity.config.security.handler.AjaxAuthenticationFailureHandler;
+import io.security.corespringsecurity.config.security.handler.AjaxAuthenticationSuccessHandler;
 import io.security.corespringsecurity.config.security.provider.AjaxAuthenticationProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Slf4j
 public class AjaxSecurityConfig {
     @Autowired
-    private AjaxLoginProcessingFilter ajaxLoginProcessingFilter;
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Autowired
     public void globalConfigure(AuthenticationManagerBuilder auth, AjaxAuthenticationProvider provider) {
@@ -32,9 +35,18 @@ public class AjaxSecurityConfig {
                         authorizeHttpRequests
                                 .anyRequest().authenticated()
                 )
-                .addFilterBefore(ajaxLoginProcessingFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
                 .csrf((csrf) -> csrf.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+        AjaxLoginProcessingFilter filter = new AjaxLoginProcessingFilter();
+        filter.setAuthenticationManager(authenticationConfiguration.getAuthenticationManager());
+        filter.setAuthenticationSuccessHandler(new AjaxAuthenticationSuccessHandler());
+        filter.setAuthenticationFailureHandler(new AjaxAuthenticationFailureHandler());
+        return filter;
     }
 }
--- src/main/java/io/security/corespringsecurity/config/security/filter/AjaxLoginProcessingFilter.java
+++ src/main/java/io/security/corespringsecurity/config/security/filter/AjaxLoginProcessingFilter.java
@@ -6,23 +6,19 @@ import io.security.corespringsecurity.domain.AccountDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
-@Component
 public class AjaxLoginProcessingFilter extends AbstractAuthenticationProcessingFilter {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public AjaxLoginProcessingFilter(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AjaxLoginProcessingFilter() {
         super(new AntPathRequestMatcher("/api/login"));
-        setAuthenticationManager(authenticationConfiguration.getAuthenticationManager());
     }
 
     @Override
